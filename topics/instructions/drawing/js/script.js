322a8935819349962fdc752165ef47b165a706e3
Pulchtritude <jhannawesome@gmail.com>
2026-09-15 17:24:25 -0400

    Implemented the Greatest Record of All Time

--- topics/instructions/drawing/js/script.js
+++ topics/instructions/drawing/js/script.js
@@ -1,24 +1,47 @@
 /**
- * Title of Project
- * Author Name
+ * Waltz for Debby by The Bill Evans Trio on vinyl (the Greatest Record of All Time)
+ * John Hanna
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Displays the greatest record of all time
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Creates a square canvas
 */
 function setup() {
-
+    createCanvas(640, 640);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Displays the record
 */
 function draw() {
+    // Grey Background
+    background(150);
+
+    // Main part of the record
+    push();
+    fill(250, 0, 0);
+    stroke(255);
+    ellipse(320, 320, 480);
+    pop();
+
+    // The Label on the Record
+    push();
+    fill("White");
+    noStroke();
+    ellipse(320, 320, 140);
+    pop();
+
+    // The hole of the record
+    push();
+    fill("#000000");
+    noStroke();
+    ellipse(320, 320, 20, 20)
+    pop();
+
 
 }
