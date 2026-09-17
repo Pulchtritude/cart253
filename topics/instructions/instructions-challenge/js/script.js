50a5f2be7b08e66936ff1ce189e725af0a88bde6
Pulchtritude <jhannawesome@gmail.com>
2026-09-17 16:09:47 -0400

    I made a landscape, it's pretty cool I guess I don't know :)

--- topics/instructions/instructions-challenge/js/script.js
+++ topics/instructions/instructions-challenge/js/script.js
@@ -1,9 +1,8 @@
 /**
- * Title of Project
+ * Landscape
  * Author Name
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * A landscape; of what exactly, I haven't decided yet
  */
 
 "use strict";
@@ -12,7 +11,7 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-
+    createCanvas(640, 640);
 }
 
 
@@ -20,5 +19,68 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+
+    // Grey background
+    background(150);
+
+    //Array of towers
+    drawTower(10, 15);
+
+    drawTower(130, 40);
+
+    drawTower(165, 80);
+
+    drawTower(90, 90);
+
+    drawTower(290, 90);
+
+    drawTower(225, 90);
+
+    drawTower(570, 70);
+
+    drawTower(440, 20);
+
+    drawTower(260, 40);
+
+    drawTower(40, 60);
+
+    drawTower(200, 90);
+
+    drawTower(510, 90);
+
+    drawTower(540, 90);
+
+    drawTower(330, 120);
+
+    drawTower(390, 80);
+
+    drawTower(470, 40);
+
+    drawTower(600, 20);
+
+    //The Landmass
+    push();
+    fill("#000000");
+    stroke("#000000");
+    ellipse(320, 0, 640, 300);
+    pop();
+
+    //Autre Landmass
+    push();
+    fill("#ff0000");
+    triangle(320, 0, 640, 300);
+    pop();
+
+
+}
+
+function drawTower(x, y) {
+
+    //Towers
+    push();
+    fill("#ffffff");
+    stroke("#000000");
+    rect(x, y, 20, 200);
+    pop();
 
 }
